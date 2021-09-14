f9776f0daf687255027fecfa646d3d2774c853e3
Mado Nanika <nanikamado@gmail.com>
2021-09-14 15:37:55 +0900

    Rename functions

--- test/fibonacci.js
+++ test/fibonacci.js
@@ -1,14 +1,14 @@
 {
-    let plusOp = a => b => {
+    let $plus = a => b => {
         return a + b;
     };
-    let minusOp = a => b => {
+    let $minus = a => b => {
         return a - b;
     };
     let fib = (a0) => {
         return (a0 == 0)
             ? 0 : (a0 == 1)
-                ? 1 : (plusOp(fib(minusOp(a0)(1)))(fib(minusOp(a0)(2))));
+                ? 1 : ($plus(fib($minus(a0)(1)))(fib($minus(a0)(2))));
     };
     let main = (a0) => {
         return (a0 === '()') ?
